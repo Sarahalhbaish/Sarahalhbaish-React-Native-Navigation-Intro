544782d4bdccf566a0cb4bb5052346df606885d6
Sarahalhbaish <sarah21995@live.com>
2024-12-25 21:40:03 +0300

    Finish the Task

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -1,13 +1,35 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import { View, TextInput, TouchableOpacity, Text, Image } from "react-native";
+import React, { useState } from "react";
 
 const EditProfile = ({ route }) => {
   const profile = route.params;
+  const [text, setText] = useState(profile.name);
+
+  const handleSave = () => {
+    console.log("Saved Name:", text);
+  };
 
   return (
     <View>
-      <Image source={profile.image} style={{ width: 160, height: 160 }} />
-      <Text style={{ fontSize: 18 }}>{profile.name}</Text>
+      <View>
+        <Image source={profile.image} style={{ width: 160, height: 160 }} />
+        <TextInput
+          placeholder="Edit your name"
+          onChangeText={(newText) => setText(newText)}
+        />
+      </View>
+      <View>
+        <TouchableOpacity
+          style={{
+            padding: 10,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onPress={handleSave}
+        >
+          <Text>Save</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
